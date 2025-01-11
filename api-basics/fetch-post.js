396a3c2959a0e5afcece1b69f438ed8ffd7b4367
Muhammad Dzaky Rafliansyah <104545005+raflidzaky@users.noreply.github.com>
2025-01-11 20:51:01 +0700

    Update fetch-post.js

--- api-basics/fetch-post.js
+++ api-basics/fetch-post.js
@@ -14,3 +14,11 @@ fetch('http://[LOCALHOST-ROUTE]/feature', {
 .then(response => response.json())  // Parse the response as JSON
 .then(data => console.log(data))     // Log the response to the console
 .catch(error => console.error('Error:', error));  // Handle any errors
+
+// Since Javascript needs a runtime environment to run on client-side for any HTTP requests 
+// To make HTTP requests from local server to client-side, I need runtime called Node.js
+// But, I could also just copy my Javascript on Chrome console, since it provides runtime
+// However, we have to copy and paste back-and-forth whenever we wants different requests
+// It is a best practice to do it on Node.js
+// Node.js is not only for HTTP requests, but also running outside the browser.
+// To trigger and automate, use Makefile all the way through
